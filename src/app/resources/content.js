e4852a3b95b0aaea990cff3800010d689bb4505f
Iacopo Paolucci <86833020+JxzePiNo@users.noreply.github.com>
2025-01-26 20:32:11 +0100

    Update content.js

--- src/app/resources/content.js
+++ src/app/resources/content.js
@@ -6,10 +6,10 @@ const person = {
   get name() {
     return `${this.firstName} ${this.lastName}`;
   },
-  role: "Design Engineer",
+  role: "Software Engineer",
   avatar: "/images/avatar.jpg",
-  location: "Asia/Jakarta", // Expecting the IANA time zone identifier, e.g., 'Europe/Vienna'
-  languages: ["English", "Bahasa"], // optional: Leave the array empty if you don't want to display languages
+  location: "Europe/Rome", // Expecting the IANA time zone identifier, e.g., 'Europe/Vienna'
+  languages: ["English", "Italian"], // optional: Leave the array empty if you don't want to display languages
 };
 
 const newsletter = {
